8a8c2b38108f986203948d697a10fb07b8f08477
mecatralha <waynealbuquerque17@gmail.com>
2021-08-14 21:08:23 -0300

    resposta ao ok

--- Impressora.py
+++ Impressora.py
@@ -6,13 +6,13 @@ com = ""
 
 ports = list(serial.tools.list_ports.comports())
 for p in ports:
-    print(p[0],p[1])
+    #print(p[0],p[1])
     if("USB Serial" in p[1]):
         com  = p[0]
 
 #com = "/dev/ttyUSB0" 
 
-print(com)
+#print(com)
 
 ser = serial.Serial()
 ser.baudrate = 115200
@@ -23,9 +23,23 @@ ser.write(b'\r\n\r\n')
 sleep(2)
 ser.flushInput()
 
-ser.write(bytes('G0 X0\n', 'utf-8'))
+lista = ['G0 X50\n', 'G0 Y50\n', 'G0 X0\n', 'G0 Y0\n']
 
-leitura = ser.readline()
-print(leitura)
+ser.write(bytes(lista[2], 'utf-8'))
+
+leitura = 'ok'
+
+for i in range(5):
+    for i in lista:
+        #leitura = ser.readlines()
+        #print(leitura)
+
+        if ('ok' in leitura):
+            ser.write(bytes(i, 'utf-8'))
+            print(leitura)
+
+        leitura = str(ser.readline())
+        #print(leitura)
+
 
 ser.close()
